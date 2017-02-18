7d66abafc5ed663b68c79f67fa2c8fe2c6860b5e
diego91964 <diego.silva.facom@gmail.com>
2017-02-18 16:27:36 -0200

    Início adição aula

---
+++ disciplinas/arquitetura-de-computadores/js/init.js
@@ -0,0 +1,45 @@
+$( document ).ready(function() {
+   Reveal.initialize({
+        controls: true,
+        progress: true,
+        history: true,
+        center: true,
+        //width: 1024,
+        //height: 768,
+        width: 960,
+        height: 700,
+        transition: 'slide', // none/fade/slide/convex/concave/zoom
+        // Optional reveal.js plugins
+        dependencies: [{
+            src: 'lib/js/classList.js',
+            condition: function() {
+                return !document.body.classList;
+            }
+        }, {
+            src: 'plugin/markdown/marked.js',
+            condition: function() {
+                return !!document.querySelector('[data-markdown]');
+            }
+        }, {
+            src: 'plugin/markdown/markdown.js',
+            condition: function() {
+                return !!document.querySelector('[data-markdown]');
+            }
+        }, {
+            src: 'plugin/highlight/highlight.js',
+            async: true,
+            condition: function() {
+                return !!document.querySelector('pre code');
+            },
+            callback: function() {
+                hljs.initHighlightingOnLoad();
+            }
+        }, {
+            src: 'plugin/zoom-js/zoom.js',
+            async: true
+        }, {
+            src: 'plugin/notes/notes.js',
+            async: true
+        }]
+    });
+});
