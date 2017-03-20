0aef0c71027c39b82e46940f32e108f6f5545155
Diego <diego.silva.facom@gmail.com>
2017-03-20 18:08:33 -0300

    Correção de exercícios

---
+++ disciplinas/tecnicas-de-programacao/c-codes/cap8/05.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+
+int main ()
+{
+    int num;
+    printf ("Entre com um numero: ");
+    scanf ("%d",&num);
+    printf ("\n\n");
+    printf ("O numero digitado e %d",num);
+    return(0);
+}
---
+++ disciplinas/tecnicas-de-programacao/c-codes/cmake-build-debug/CMakeFiles/c_codes.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/c_codes.dir/cap8/05.c.o"
+  "c_codes.pdb"
+  "c_codes"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/c_codes.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
