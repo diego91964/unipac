9aa68bc8aa33e016030641a3631faa5b8cf852dc
Diego <diego.silva.facom@gmail.com>
2017-05-07 19:31:27 -0300

    Adicionando resolução de exercicio do fatorial

---
+++ disciplinas/tecnicas-de-programacao/c-codes/fatorial/fatorial.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main () {
+
+int resultado, entrada, i;
+
+resultado = 1;
+
+scanf("%d", &entrada);
+
+for (i = 0 ; i < entrada ; i++ ){
+  resultado = resultado * (entrada - i);
+}
+
+printf ("%d", resultado);
+
+
+}
---
+++ disciplinas/tecnicas-de-programacao/c-codes/fatorial/fatorial_recursivo.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+int fact (int fa){
+
+        if (fa == 0){ return 0;}
+        else if (fa == 1) {return 1;}
+        else {return fa*fact(fa -1);}
+}
+
+
+
+int main () { 
+
+
+int resultado, entrada, i;
+
+resultado = 1;
+
+scanf("%d", &entrada);
+
+ resultado = fact (entrada);
+
+printf ("%d", resultado);
+
+}
+
